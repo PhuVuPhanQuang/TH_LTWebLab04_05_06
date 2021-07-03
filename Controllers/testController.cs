53df8d2690b64ad702ff785a737468c5fee7a4ac
Ryder_Martinez <vuphanquangphu2322@gmail.com>
2021-07-03 09:02:20 +0700

    update

---
+++ Controllers/testController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TH_LTWebLab04_05_06.Controllers
+{
+    public class testController : Controller
+    {
+        // GET: test
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
